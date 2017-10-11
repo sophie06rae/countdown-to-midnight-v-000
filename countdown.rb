4389685263249921380f7852954bfe2a5027b63d
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-11 09:28:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(n)
   while n > 0
   puts "#{n} SECOND(S)!"
   n -= 1
-    sleep(1.second)
+    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
